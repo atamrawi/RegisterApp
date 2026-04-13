ff4ff4c008ad93dbd1c71eb3dd6ef5024834e28b
Ahmed Tamrawi <ahmedtamrawi@gmail.com>
2026-04-13 14:48:45 -0500

    Update MongoDB connection environment variable

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5500;
 
 // db connection
-mongoose.connect(process.env.DB_URL,{
+mongoose.connect(process.env.MONGODB_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
